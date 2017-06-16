fbf1c775cd89e85ffbd3df142faab466533dee7c
Kyle Aleshire <kjaleshire@gmail.com>
2017-06-16 00:02:05 -0500

    Threads wip

--- n_queens_threaded.cpp
+++ n_queens_threaded.cpp
@@ -4,33 +4,36 @@
 #include <vector>
 #include <thread>
 #include <atomic>
+#include <queue>
+#include <mutex>
 
 #ifdef _MSC_VER
 #include <intrin.h>
 #pragma intrinsic(_BitScanForward)
 #endif
 
-#define N_QUEENS 10
-#define NO_PRINT 1
+#define N_QUEENS 4
+// #define NO_PRINT 1
 #define THREADS 8
 
-struct SolutionState {
+class SolutionState {
+
+public:
   std::shared_ptr<SolutionState> lastState;
   std::bitset<N_QUEENS> allowedState;
-  unsigned long attackLeft;
-  unsigned long attackRight;
-  unsigned long attackCenter;
+  int attackLeft;
+  int attackRight;
+  int attackCenter;
   unsigned long queenIndex;
-  unsigned long counter;
+  int counter;
 
   SolutionState(std::shared_ptr<SolutionState> s, std::bitset<N_QUEENS> a,
-                unsigned long l, unsigned long r, unsigned long c,
-                unsigned long q, unsigned long co) : lastState(s),
+                int l, int r, int c, unsigned long q, int co) : lastState(s),
                 allowedState(a), attackLeft(l), attackRight(r), attackCenter(c),
                 queenIndex(q), counter(co) {}
 };
 
-void printRow(unsigned long index) {
+void printRow(int index) {
   for (auto i = 0; i < (N_QUEENS - 1 - index); i++) {
     std::cout << "_ ";
   }
@@ -45,7 +48,7 @@ void printRow(unsigned long index) {
 void printSolution(std::shared_ptr<SolutionState> state) {}
 #else
 void printSolution(std::shared_ptr<SolutionState> state) {
-  std::vector<unsigned long> queenIndexes;
+  std::vector<int> queenIndexes;
 
   std::shared_ptr<SolutionState> s = state;
   while(s != nullptr) {
@@ -60,48 +63,72 @@ void printSolution(std::shared_ptr<SolutionState> state) {
 }
 #endif
 
-void spawnWorker(std::shared_ptr<SolutionState> state, std::atomic_int& solutionCounter) {
-  // win
-  if (state->counter == N_QUEENS - 1 && state->allowedState.any()) {
-    printSolution(state);
+void worker(std::atomic_int& solutionCounter, std::queue<std::shared_ptr<SolutionState>>& workQueue, std::mutex& qTex) {
+  while(true) {
+    qTex.lock();
+    std::shared_ptr<SolutionState> state = workQueue.front();
+    workQueue.pop();
+    qTex.unlock();
 
-    solutionCounter++;
-    return;
-  }
+    // no work left, quit
+    if(state == nullptr)
+      break;
 
-  auto attackMask = state->allowedState.to_ulong();
+    // win
+    if (state->counter == N_QUEENS - 1 && state->allowedState.any()) {
+      printSolution(state);
 
-  auto attackLeft = (attackMask | state->attackLeft) >> 1;
-  auto attackRight = (attackMask | state->attackRight) << 1;
-  auto attackCenter = attackMask | state->attackCenter;
+      solutionCounter++;
+      return;
+    }
 
-  auto newState = std::make_shared<SolutionState>(state, 0, attackLeft, attackRight, attackCenter, 0, state->counter + 1);
+    auto attackMask = state->allowedState.to_ulong();
 
-  std::bitset<N_QUEENS> newAllowedState = ~(newState->attackLeft | newState->attackRight | newState->attackCenter);
+    auto attackLeft = (attackMask | state->attackLeft) >> 1;
+    auto attackRight = (attackMask | state->attackRight) << 1;
+    auto attackCenter = attackMask | state->attackCenter;
 
-  while(newAllowedState.any()) {
-#ifdef _MSC_VER
-    _BitScanForward(&newState->queenIndex, newAllowedState.to_ulong());
-#else
-    newState->queenIndex = __builtin_ctz(newAllowedState.to_ulong());
-#endif
-    newAllowedState.set(newState->queenIndex, 0);
-    newState->allowedState = 1 << newState->queenIndex;
+    auto newState = std::make_shared<SolutionState>(state, 0, attackLeft, attackRight, attackCenter, 0, state->counter + 1);
 
-    spawnWorker(newState, solutionCounter);
+    std::bitset<N_QUEENS> newAllowedState = ~(newState->attackLeft | newState->attackRight | newState->attackCenter);
+
+    while(newAllowedState.any()) {
+  #ifdef _MSC_VER
+      _BitScanForward(&newState->queenIndex, newAllowedState.to_ulong());
+  #else
+      newState->queenIndex = __builtin_ctz(newAllowedState.to_ulong());
+  #endif
+      newAllowedState.set(newState->queenIndex, 0);
+      newState->allowedState = 1 << newState->queenIndex;
+
+      qTex.lock();
+      workQueue.push(newState);
+      qTex.unlock();
+    }
   }
 }
 
 int main(int argc, char **argv) {
   std::atomic_int solutionCounter(0);
+  std::queue<std::shared_ptr<SolutionState>> workQueue;
+  std::mutex qTex;
+  std::vector<std::thread> threadHandles;
 
   for(auto i = 0; i < N_QUEENS; i++) {
     auto state = std::make_shared<SolutionState>(nullptr, 0, 0, 0, 0, 0, 0);
     state->queenIndex = i;
     state->allowedState = 1 << i;
 
-    spawnWorker(state, solutionCounter);
+    workQueue.push(state);
   }
+
+  for(auto i = 0; i < 1; i++) {
+    std::thread t(worker, std::ref(solutionCounter), std::ref(workQueue), std::ref(qTex));
+    threadHandles.push_back(std::move(t));
+  }
+
+  for(auto& t : threadHandles)
+    t.join();
 
   // done
   std::cout << "Solutions found: " << solutionCounter << std::endl;
